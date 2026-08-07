77f92e84b98b3dd5df308a50303c3fd54468e48a
Enrico Tassi <Enrico.Tassi@Inria.fr>
2026-08-07 15:12:52 +0200

    wip

--- extract_code.py
+++ extract_code.py
@@ -65,6 +65,7 @@ def clean_line_global(l,escape):
     l = re.sub(r"\bOK\b", "Top", l)
     l = re.sub(r"\bKO\b", "Bot", l)
     l = re.sub(r"\bpath_atom\b", "incomplete", l)
+    l = re.sub(r"\bacyclic_sigma\b", "acyclic", l)
     l = re.sub(r"\bget_subst\b", "next_subst", l)
     l = re.sub(r"\bpath_end\b", "next_tree", l)
     l = re.sub(r"\bget_end\b", "next", l)
